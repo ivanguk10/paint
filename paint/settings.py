4bfec5e4ce1cffcdcfe90f6c67f6f312e8c18977
ivanguk10 <vanyaguk135@gmail.com>
2020-10-09 11:06:30 +0300

    hello3

--- paint/settings.py
+++ paint/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'bnp2jp+n(u-8y5hp^0of_9!ice0*vizp-qva7(*-7n%1+z&5#='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['34.65.94.175 ']
+ALLOWED_HOSTS = ['35.205.233.37 ']
 
 
 # Application definition
